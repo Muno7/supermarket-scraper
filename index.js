6bc9944a3b669f4fad50bcc6e48bd81f94b78303
Muhammad Notfaji <mohammadnoutfaji1234@gmail.com>
2025-09-24 19:47:55 +0200

    Bug fix. Wait for show all buttons to appear before clicking them.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import puppeteer from 'puppeteer';
 
+// Create a delay using a Promise
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
 async function getHTML() {
     const browser = await puppeteer.launch({
     headless: false // Opens visible browser
@@ -8,12 +11,11 @@ async function getHTML() {
     const page = await browser.newPage();
     await page.goto('https://www.coop.se/butiker-erbjudanden/coop/coop-skurup/');
 
+    await page.waitForSelector('div.Rnj2piQi button.Wy2NiM6K.tkw4k9hc.dIzgXmHF'); /* wait for the visa alla buttons to appear */
     await page.evaluate(() => {
         const buttons = document.querySelectorAll('div.Rnj2piQi button.Wy2NiM6K.tkw4k9hc.dIzgXmHF');
         buttons.forEach(button => button.click());
-    });
-
-    await page.waitForSelector('div.ProductTeaser');
+    }); /* click all visa alla buttons to make all products appear on the page */
 
     const productTeasers = await page.$$('div.ProductTeaser');
 
