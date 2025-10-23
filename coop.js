ec418bece4f4dddd3cb8f47efcb441a608b2f9ed
Muno7 <mohammadnoutfaji1234@gmail.com>
2025-10-23 11:23:56 +0200

    Fixed code for scraping products without variants since coop changed their site

--- coop.js
+++ coop.js
@@ -15,7 +15,7 @@ async function getProducts() {
         buttons.forEach(button => button.click());
     }); // click all visa alla buttons to make all products appear on the page
 
-    const productElements = await page.$$('div.ProductTeaser');
+    const productElements = await page.$$('article.ohKiwh8z.xSu5Uapq');
 
     for (const productElement of productElements) {
         await scrapeProductData(productElement, false);
@@ -57,27 +57,37 @@ async function scrapeProductVariants(productElement) {
 
 async function scrapeProductData(productElement, isVariant) {
   const productData = await productElement.evaluate(element => {
-    const title = element.querySelector('h3.ProductTeaser-heading')?.textContent?.trim() || null;
-    const price = element.querySelector('div.OXm1GQVM')?.textContent?.trim() || element.querySelector('div.lQkA89R')?.textContent?.trim() || null;
-    const cents = element.querySelector('.uHKGEzxo')?.textContent?.trim() || element.querySelector('.W4Xpjgmp')?.textContent?.trim() || null;
-    const unit = element.querySelector('.ew9hNB6_')?.textContent?.trim() || element.querySelector('.kk5N_OPI')?.textContent?.trim() || null;
-    const deal =  element.querySelector('.QUeU6xhZ')?.textContent?.trim() || null;
-    const size =  element.querySelector('.EnRQ98Sx.Nk9lLHfd.ahPAzIfS')?.textContent?.trim() || null;
-    const brand =  element.querySelector('div.ProductTeaser-brand')?.textContent?.trim() || null;
-    const comparisonPrice = element.querySelector('.nEatcoiG :nth-of-type(3)')?.textContent?.trim() || null;
-    const image = element.querySelector('div.ProductTeaser-image img')?.src || null;
-    const isMember = element.querySelector('.SlFPVeq_') ? true : false;
-    const badges = Array.from(element.querySelectorAll('.ProductTeaser-badges img')).map(img => img.src) || null;
-    const hasVariants = element.querySelector('.Wy2NiM6K.iyTkejCQ.nZnVqEJd.mOvy16Ff') !== null;
+    const title = element.querySelector('.OXTlHT32')?.textContent?.trim() || null;
+    const price = element.querySelector('.slH8Imgo span:nth-last-child(2)')?.textContent?.trim() || Array.from(element.querySelector('.n1OznkM1')?.childNodes).filter(node => node.nodeType === Node.TEXT_NODE)[0]?.textContent?.trim() || null;
+    const unit = element.querySelector('.Oam2mkaA :nth-of-type(2)')?.textContent?.trim() || null;
+    const deal =
+        element.querySelector('.slH8Imgo')?.querySelector('.u-textMedium')
+            ? element.querySelector('.slH8Imgo :nth-of-type(1)')?.textContent?.trim()
+            : element.querySelector('.n1OznkM1')?.querySelector('.u-textMedium')
+            ? element.querySelector('.n1OznkM1 :nth-of-type(1)')?.textContent?.trim()
+            : null;
+    const size = (() => {
+        const span = element.querySelector('.uLmN8HjX span:nth-last-child(1)');
+        return span && !span.classList.contains('q5vMS42j') && span.classList.length === 0
+            ? span.textContent.trim()
+            : null;
+    })();
+    const brand =  element.querySelector('.q5vMS42j')?.textContent?.trim() || null;
+    const max = element.querySelector('.UWFn16pY div.u-colorRed1')?.textContent?.trim() || null;
+    const comparisonPrice = element.querySelector('.UWFn16pY div:nth-last-child(1)')?.textContent?.trim() || null;
+    const image = element.querySelector('.q_ToRdGh.FJbxCHJa img')?.src || null;
+    const isMember = element.querySelector('.aP5TkjG6.Er3da50N') ? true : false;
+    const badges = Array.from(element.querySelectorAll('.VFIPn1KI img')).map(img => img.src) || null;
+    const hasVariants = element.querySelector('.Wy2NiM6K.iyTkejCQ._yrJb637.mOvy16Ff') !== null
         
     return {
       title,
       price,
-      cents,
       unit,
       deal,
       size,
       brand,
+      max,
       comparisonPrice,
       image,
       isMember,
@@ -86,11 +96,6 @@ async function scrapeProductData(productElement, isVariant) {
     };
   });
   
-    // if the product has variants and itself is not variant of another product then scrape the variants of that product
-    if (!isVariant && productData.hasVariants) {
-        productData.variants = await scrapeProductVariants(productElement);
-        delete productData.hasVariants;
-    }
 
     // if the product is not a variant of another product then add the product as its own product in json
     if (!isVariant) {
@@ -107,7 +112,7 @@ async function pushProductsToJson(productData) {
     products.push(productData);
   
     // Save to JSON file
-    fs.writeFileSync('products.json', JSON.stringify(products, null, 2));
+    fs.writeFileSync('coop.json', JSON.stringify(products, null, 2));
 
 }
 
